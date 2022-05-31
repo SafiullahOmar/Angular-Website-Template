c156193895f9967f3c8231321656a0b3dd92b919
Safiullah Omar <omar.safiullah@gmail.com>
2022-05-31 12:10:18 +0430

    Final Website Template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output ,EventEmitter} from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+@Output() SideNavToggled=new EventEmitter<boolean>();
+menuStatus:boolean=false;
+constructor() { }
 
   ngOnInit(): void {
+  }
+
+  SideNavToggle(){
+    this.menuStatus=!this.menuStatus;
+    this.SideNavToggled.emit(this.menuStatus);
   }
 
 }
--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
@@ -6,8 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent implements OnInit {
-
+@Input() SideNavStatus:boolean=false;
   constructor() { }
+
+  list=[
+    {
+      number:'1',
+      name:'Home',
+      icon:'fa-solid fa-house'
+    },
+    {
+      number:'2',
+      name:'Analytics',
+      icon:'fa-solid fa-chart-line'
+    },
+    {
+      number:'3',
+      name:'Products',
+      icon:'fa-solid fa-box'
+    }
+  ];
 
   ngOnInit(): void {
   }
